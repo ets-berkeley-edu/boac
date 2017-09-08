deba5b0ca021a3da35506fb3948a095689f031f4
Ray Davis <ray@media.berkeley.edu>
2017-09-08 13:07:17 -0700

    Include Canvas user profile in api/status endpoint

--- boac/api/status_controller.py
+++ boac/api/status_controller.py
@@ -1,15 +1,24 @@
 from flask import current_app as app, jsonify
 from flask_login import current_user
 
+from boac.externals import canvas
+
 @app.route('/api/status')
 def app_status():
+    uid = current_user.get_id()
     authn_state = {
         'is_authenticated': current_user.is_authenticated,
         'is_active': current_user.is_active,
         'is_anonymous': current_user.is_anonymous,
-        'uid': current_user.get_id()
+        'uid': uid
     }
-    # TODO Include Canvas profile if currently authenticated.
-    return jsonify({
+    resp = {
         'authenticated_as': authn_state
-    })
+    }
+
+    if current_user.is_active:
+        canvas_response = canvas.get_user_for_sis_id(app.canvas_instance, uid)
+        if canvas_response:
+            resp['canvas_profile'] = canvas_response.json()
+
+    return jsonify(resp)
--- tests/test_api/test_status_controller.py
+++ tests/test_api/test_status_controller.py
@@ -1,5 +1,6 @@
 class TestStatusController:
     """Status API"""
+
     def test_anonymous_status(self, client):
         """returns a well-formed response"""
         response = client.get('/api/status')
@@ -12,6 +13,11 @@ class TestStatusController:
         fake_auth.login(test_uid)
         response = client.get('/api/status')
         assert response.status_code == 200
-        assert response.json['authenticated_as']
         assert response.json['authenticated_as']['is_authenticated']
         assert response.json['authenticated_as']['uid'] == test_uid
+
+    def test_includes_canvas_profile_if_available(self, client, fake_auth):
+        test_uid = '2040'
+        fake_auth.login(test_uid)
+        response = client.get('/api/status')
+        assert response.json['canvas_profile']['sis_login_id'] == test_uid
