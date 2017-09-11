655b9110fcacc07b019c29984a25b987e1e042be
Ray Davis <ray@media.berkeley.edu>
2017-09-11 13:00:13 -0700

    NOJIRA Decouple authentication from Canvas API calls

---
+++ boac/api/user_controller.py
@@ -0,0 +1,23 @@
+from boac.externals import canvas
+from flask import current_app as app, jsonify
+from flask_login import current_user
+
+
+@app.route('/api/profile')
+def user_profile():
+    canvas_profile = False
+    if current_user.is_active:
+        uid = current_user.get_id()
+        canvas_response = canvas.get_user_for_sis_id(app.canvas_instance, uid)
+        if canvas_response:
+            canvas_profile = canvas_response.json()
+        elif (canvas_response.raw_response is None) or (canvas_response.raw_response.status_code != 404):
+            canvas_profile = {
+                'error': 'Unable to reach bCourses',
+            }
+    else:
+        uid = False
+    return jsonify({
+        'uid': uid,
+        'canvas_profile': canvas_profile,
+    })
---
+++ tests/test_api/test_user_controller.py
@@ -0,0 +1,36 @@
+import boac.externals.canvas as canvas
+from boac.lib.mockingbird import MockResponse, register_mock
+
+
+class TestUserController:
+    """User Profile API"""
+
+    def test_profile_not_authenticated(self, client):
+        """returns a well-formed response"""
+        response = client.get('/api/profile')
+        assert response.status_code == 200
+        assert not response.json['uid']
+
+    def test_profile_includes_lack_of_canvas_account(self, client, fake_auth):
+        test_uid = '1133399'
+        fake_auth.login(test_uid)
+        response = client.get('/api/profile')
+        assert response.status_code == 200
+        assert response.json['uid'] == test_uid
+        assert response.json['canvas_profile'] == False
+
+    def test_profile_includes_message_if_canvas_failure(self, client, fake_auth):
+        test_uid = '1133399'
+        fake_auth.login(test_uid)
+        canvas_error = MockResponse(500, {}, '{"message": "Internal server error."}')
+        with register_mock(canvas.get_user_for_sis_id, canvas_error):
+            response = client.get('/api/profile')
+            assert response.status_code == 200
+            assert response.json['uid'] == test_uid
+            assert response.json['canvas_profile']['error']
+
+    def test_includes_canvas_profile_if_available(self, client, fake_auth):
+        test_uid = '2040'
+        fake_auth.login(test_uid)
+        response = client.get('/api/profile')
+        assert response.json['canvas_profile']['sis_login_id'] == test_uid
