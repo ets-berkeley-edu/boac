87939b8018ba065f30b81be2934b124598d50f51
John Crossman <johncrossman@berkeley.edu>
2017-09-12 20:38:50 -0700

    BOAC-45, 'config' constant available in templates; module value 'me' refreshed on logOut

--- boac/static/js/factories/authFactory.js
+++ boac/static/js/factories/authFactory.js
@@ -2,21 +2,39 @@
 
   'use strict';
 
-  angular.module('boac').factory('authFactory', function($http) {
-    var devAuthLogIn = function(uid, password) {
-      return $http.post('/devauth/login', {
-        uid: uid,
-        password: password
+  var boac = angular.module('boac');
+
+  boac.factory('authFactory', function(me, $http, $rootScope) {
+
+    var refreshStatus = function() {
+      return $http.get('/api/status').then(function(results) {
+        var data = results.data;
+        // Refresh module value in case other controllers, factories, etc. do a lookup in the current page context.
+        boac.value('me', data);
+        // Refresh instance currently referenced in templates
+        $rootScope.me = data;
+        return;
       });
     };
 
-    var status = function() {
-      return $http.get('/api/status');
+    var devAuthLogIn = function(uid, password) {
+      var credentials = {
+        uid: uid,
+        password: password
+      };
+      return $http.post('/devauth/login', credentials).then(refreshStatus);
     };
+
+    var logOut = function() {
+      return $http.get('/logout').then(refreshStatus);
+    };
+
+    // Make current user available to templates
+    $rootScope.me = me;
 
     return {
       devAuthLogIn: devAuthLogIn,
-      status: status
+      logOut: logOut
     };
   });
 
