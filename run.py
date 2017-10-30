4e5498caab426f08a2574034d84e14e03284c99c
Paul Kerschen <kerschen@berkeley.edu>
2017-10-30 12:27:21 -0700

    BOAC-109 Special handling for environment variables under WSGI

--- run.py
+++ run.py
@@ -12,7 +12,20 @@ Usage mode B:
 >>> flask initdb
 """
 
+import os
+import subprocess
+
 from boac.factory import create_app
+
+# When running under WSGI, system environment variables are not automatically made available to Python code, and
+# an app restart will result in configurations being lost. We work around this with an explicit load from the shell
+# environment, sourcing from the Elastic Beanstalk-provided /opt/python/current/env file if available.
+if os.environ.get('HOME') == '/home/wsgi':
+    command = ['bash', '-c', '(source /opt/python/current/env || true) && env']
+    shell_environment = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for line in shell_environment.stdout:
+        key, _, value = line.decode('utf-8').rstrip().partition('=')
+        os.environ[key] = value
 
 application = create_app()
 
