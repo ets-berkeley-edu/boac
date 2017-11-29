af9be11fee3762d2eb63f606a273b7559dd1f6f8
John Crossman <johncrossman@berkeley.edu>
2017-11-28 17:04:28 -0800

    BOAC-158, create cohort (modal); no pagination for teams; lazy load matrix

---
+++ boac/static/app/cohort/createCohortController.js
@@ -0,0 +1,43 @@
+(function(angular) {
+
+  'use strict';
+
+  angular.module('boac').controller('CreateCohortController', function($scope, $uibModal) {
+
+    $scope.openCreateCohortModal = function(teams) {
+      $uibModal.open({
+        animation: true,
+        ariaLabelledBy: 'create-cohort-header',
+        ariaDescribedBy: 'create-cohort-body',
+        templateUrl: '/static/app/cohort/createCohortModal.html',
+        controller: 'CreateCohortModal',
+        resolve: {
+          teams: function() {
+            return teams;
+          }
+        }
+      });
+    };
+  });
+
+  angular.module('boac').controller('CreateCohortModal', function(teams, cohortFactory, $scope, $uibModalInstance) {
+
+    $scope.cohortName = null;
+    $scope.errorMessage = null;
+
+    $scope.create = function() {
+      $scope.cohortName = _.trim($scope.cohortName);
+      if (_.isEmpty($scope.cohortName)) {
+        $scope.errorMessage = 'Required';
+      } else {
+        cohortFactory.createCohort($scope.cohortName, _.map(teams, 'code'));
+        $uibModalInstance.close();
+      }
+    };
+
+    $scope.cancel = function() {
+      $uibModalInstance.close();
+    };
+  });
+
+}(window.angular));
