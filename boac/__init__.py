53b4cff93498ff1b123fd3cadd0cdd26a83bf835
Paul Kerschen <kerschen@berkeley.edu>
2018-01-05 13:53:31 -0800

    Standardize per-environment db commit behavior

--- boac/__init__.py
+++ boac/__init__.py
@@ -1,16 +1,20 @@
+from flask import current_app as app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
 db = SQLAlchemy()
 
 
-def std_commit():
+def std_commit(allow_test_environment=False):
     """Commit failures in SQLAlchemy must be explicitly handled. This function follows the suggested default,
     which is to roll back and close the active session, letting the pooled connection start a new transaction
     cleanly.
     WARNING: Session closure will invalidate any in-memory DB entities. Rows will have to be reloaded from the DB
     to be read or updated.
     """
+    # Give a hoot, don't pollute.
+    if app.config['TESTING'] and not allow_test_environment:
+        return
     successful_commit = False
     try:
         db.session.commit()
