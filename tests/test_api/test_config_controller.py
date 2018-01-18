d130439a2544f701d9a759503c5eb6f45757fef8
John Crossman <johncrossman@berkeley.edu>
2018-01-18 15:04:33 -0800

    BOAC-385, put EB_ENVIRONMENT in config file (03_download_local_configuration)

--- tests/test_api/test_config_controller.py
+++ tests/test_api/test_config_controller.py
@@ -6,7 +6,8 @@ class TestConfigController:
         response = client.get('/api/config')
         assert response.status_code == 200
         assert 'boacEnv' in response.json
-        # In tests, Google Analytics is integrated and disabled
+        # In tests, certain configs are omitted or disabled (e.g., Google Analytics)
+        assert response.json['ebEnvironment'] is None
         assert response.json['googleAnalyticsId'] is False
 
     def test_anonymous_api_version_request(self, client):
