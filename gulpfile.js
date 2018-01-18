ccea92ac9fcc1ee4ba753c9bfce2add172522009
John Crossman <johncrossman@berkeley.edu>
2018-01-17 17:06:07 -0800

    BOAC-251, blue skies outage page when HTTP status code 502-504

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('rev', function(done) {
   gulp.src('boac/static/lib/**/*').pipe(gulp.dest('dist/static/lib'));
   var css = filter('boac/static/app/**/*.css', {restore: true});
   // The index.html filename, unlike js/css files, is preserved
-  var index = filter(['**/*', '!**/index.html', '!**/maintenance.html'], {restore: true});
+  var index = filter(['**/*', '!**/favicon.ico', '!**/index.html'], {restore: true});
   // We cannot minify our js due to http://budiirawan.com/uglify-angular-error-unpr-unknown-provider-aprovider/
   gulp.src(['boac/static/app/**', 'boac/templates/index.html'])
     .pipe(css)
