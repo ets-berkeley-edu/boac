ff6a4548a0f81e2f0e7f4068651d4318507b5229
John Crossman <johncrossman@berkeley.edu>
2018-01-31 17:31:13 -0800

    BOAC-454/BOAC-462, fa-plus/fa-minus for myList add/remove; no icon in h1 and h3 please

--- boac/static/app/user/watchlistToggleDirective.js
+++ boac/static/app/user/watchlistToggleDirective.js
@@ -4,10 +4,6 @@
 
   angular.module('boac').directive('watchlistToggle', function(watchlistFactory, $rootScope) {
 
-    var getAlertMessage = function(onWatchlist, sid) {
-      return onWatchlist ? 'Remove student ' + sid + ' from my list.' : 'Add student ' + sid + ' to my list.';
-    };
-
     return {
       // @see https://docs.angularjs.org/guide/directive#template-expanding-directive
       restrict: 'E',
@@ -21,10 +17,18 @@
       templateUrl: '/static/app/user/watchlistToggle.html',
 
       link: function(scope, elem, attrs) {
-
         scope.faSize = attrs.faSize;
         scope.onWatchlist = !!_.find(scope.watchlist, ['sid', scope.sid]);
-        scope.alertMessage = getAlertMessage(scope.onWatchlist, scope.sid);
+
+        var buttonAltText = function() {
+          return scope.onWatchlist ? 'Remove student ' + scope.sid + ' from my list' : 'Add student ' + scope.sid + ' to my list';
+        };
+        var buttonLabel = function() {
+          return scope.onWatchlist ? 'remove from My List' : 'add to My List';
+        };
+
+        scope.buttonAltText = buttonAltText();
+        scope.buttonLabel = buttonLabel();
 
         scope.toggle = function($event) {
           $event.stopPropagation();
@@ -32,7 +36,8 @@
           // Choose proper factory method and then immediately toggle icon in UI
           var updateWatchlist = scope.onWatchlist ? watchlistFactory.removeFromWatchlist : watchlistFactory.addToWatchlist;
           scope.onWatchlist = !scope.onWatchlist;
-          scope.alertMessage = getAlertMessage(scope.onWatchlist, scope.sid);
+          scope.buttonLabel = buttonLabel();
+          scope.buttonAltText = buttonAltText();
           updateWatchlist(scope.sid).then(function() {
             return false;
           });
