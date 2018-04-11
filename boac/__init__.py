ace804d3f97e11f2d5043dfb1f361760695f7b52
John Crossman <johncrossman@berkeley.edu>
2018-04-11 15:46:35 -0700

    NOJIRA, master bumped to v2.1

--- boac/__init__.py
+++ boac/__init__.py
@@ -28,7 +28,7 @@ from flask import current_app as app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '2.0'
+__version__ = '2.1'
 
 db = SQLAlchemy()
 
