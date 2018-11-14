350b5c9baec99678371e5eadf3c2c159b2e79465
John Crossman <johncrossman@berkeley.edu>
2018-11-14 13:03:11 -0800

    BOAC-1440, fix CodeBuild failure on npm run build-vue

--- scripts/production-vuejs-post-build.sh
+++ scripts/production-vuejs-post-build.sh
@@ -3,8 +3,6 @@
 # Abort immediately if a command fails
 set -e
 
-npm run build-vue
-
 cp dist/static/vue/css/* dist/static/css/
 cp dist/static/vue/js/* dist/static/js/
 cp dist/static/vue/img/* dist/static/img/
