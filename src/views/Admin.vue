9d50b46050a062dd356fcc6998d81baf268047a6
John Crossman <johncrossman@berkeley.edu>
2018-11-30 14:05:22 -0800

    BOAC-1435, functioning become(uid) on Admin page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -4,11 +4,35 @@
     <div v-if="!loading">
       <h1>BOAC Flight Deck</h1>
       <DemoModeToggle/>
+
       <h2 class="page-section-header-sub">Users</h2>
-      <v-treeview
-          :items="userGroups"
-          item-key="code"
-          item-children="users"></v-treeview>
+      <b-card no-body>
+        <b-tabs pills card no-key-nav>
+          <b-tab :title="group.name" v-for="group in userGroups" v-bind:key="group.name">
+            <b-container fluid>
+              <b-row v-for="groupUser in group.users"
+                     v-bind:key="groupUser.uid">
+                <b-col sm="auto"
+                       class="pr-0"
+                       v-bind:class="{'pb-2': groupUser.uid === user.uid}">
+                  <a v-bind:class="{'faint-text pb-2': groupUser.uid === user.uid}"
+                     :aria-label="'Go to UC Berkeley Directory page of ' + groupUser.name"
+                     :href="'https://www.berkeley.edu/directory/results?search-term=' + groupUser.name"
+                     target="_blank">{{groupUser.name}}</a>
+                </b-col>
+                <b-col v-if="groupUser.uid !== user.uid">
+                  <b-btn :id="'become-' + groupUser.uid"
+                         class="mb-1 p-0"
+                         :title="'Log in as ' + groupUser.name"
+                         variant="link"
+                         v-on:click="become(groupUser.uid)"
+                         v-if="devAuthEnabled"><i class="fas fa-sign-in-alt"></i></b-btn>
+                </b-col>
+              </b-row>
+            </b-container>
+          </b-tab>
+        </b-tabs>
+      </b-card>
     </div>
   </div>
 </template>
@@ -19,6 +43,7 @@ import { becomeUser, getAuthorizedUserGroups } from '@/api/user';
 import DemoModeToggle from '@/components/DemoModeToggle.vue';
 import Spinner from '@/components/Spinner.vue';
 import Loading from '@/mixins/Loading.vue';
+import store from '@/store';
 
 export default {
   name: 'Admin',
@@ -34,12 +59,16 @@ export default {
     active: [],
     userGroups: []
   }),
+  computed: {
+    user: () => store.getters.user,
+    devAuthEnabled: () => _.get(store.getters.config, 'devAuthEnabled')
+  },
   methods: {
     loadUserGroups() {
       getAuthorizedUserGroups().then(data => {
         this.userGroups = data;
         _.each(this.userGroups, group => {
-          _.each(group.users, user => {
+          _.each(_.orderBy(group.users, 'name'), user => {
             user.id = user.uid;
             user.name = user.firstName + ' ' + user.lastName;
           });
@@ -47,10 +76,8 @@ export default {
         this.loaded();
       });
     },
-    selected(uid) {
-      becomeUser(uid).then(() => {
-        window.location = '/home';
-      });
+    become(uid) {
+      becomeUser(uid).then(() => (window.location = '/home'));
     }
   }
 };
