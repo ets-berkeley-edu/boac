c114e60ba7e677d80b1cbe608b4ce66a0050cb39
Paul Kerschen <kerschen@berkeley.edu>
2018-12-06 12:20:33 -0800

    BOAC-1498 Student profile (Vue): enrollment data in progress

--- src/mixins/AppConfig.vue
+++ src/mixins/AppConfig.vue
@@ -5,6 +5,8 @@ import store from '@/store';
 export default {
   name: 'AppConfig',
   computed: {
+    currentEnrollmentTermId: () =>
+      _.get(store.getters.config, 'currentEnrollmentTermId'),
     devAuthEnabled: () => _.get(store.getters.config, 'devAuthEnabled'),
     supportEmailAddress: () =>
       _.get(store.getters.config, 'supportEmailAddress')
