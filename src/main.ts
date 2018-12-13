2109ae6f1f9272f4662800124b3ab71b98c8b569
John Crossman <johncrossman@berkeley.edu>
2018-12-12 16:58:36 -0800

    BOAC-1529, move our custom Vue.filters to ./filters file; a cleaner main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import { format as formatDate, parse as parseDate } from 'date-fns';
+import './filters';
 import App from './App.vue';
 import axios from 'axios';
 import BootstrapVue from 'bootstrap-vue';
@@ -26,32 +26,6 @@ Vue.use(BootstrapVue);
 Vue.use(require('vue-lodash'));
 Vue.use(VueHighcharts);
 
-Vue.filter('date', (dateString: string, format: string = 'MMM dd, YYYY') => {
-  let date = parseDate(dateString);
-  return formatDate(date, format);
-});
-Vue.filter('lowercase', (str: string) => {
-  return str.toLowerCase();
-});
-Vue.filter(
-  'pluralize',
-  (noun: string, count: number, substitutions = {}, pluralSuffix = 's') => {
-    return (
-      `${substitutions[count] || count} ` +
-      (count !== 1 ? `${noun}${pluralSuffix}` : noun)
-    );
-  }
-);
-Vue.filter('round', function(value, decimals) {
-  return (
-    Math.round(value * Math.pow(10, decimals)) / Math.pow(10, decimals)
-  ).toFixed(decimals);
-});
-Vue.filter(
-  'variablePrecisionNumber',
-  (value, minPrecision, maxPrecision) =>
-    `TODO: ${value}, ${minPrecision}, ${maxPrecision}`
-);
 // Emit, and listen for, events via hub
 Vue.prototype.$eventHub = new Vue();
 
