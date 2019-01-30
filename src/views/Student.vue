6cd8ed5c7994e1b945ae16610f607a8fb96ee2bc
John Crossman <johncrossman@berkeley.edu>
2019-01-29 17:22:57 -0800

    BOAC-1714, finishing touches on Academic Timeline tabs

--- src/views/Student.vue
+++ src/views/Student.vue
@@ -16,7 +16,7 @@
           <StudentProfileGPA :student="student"/>
         </div>
       </div>
-      <div>
+      <div class="m-3">
         <AcademicTimeline :student="student"/>
       </div>
       <div>
