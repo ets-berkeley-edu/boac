42d911be20f6b44f883bfae9f90b7f5cafb8d1eb
Anne Lyttle <alyttle@berkeley.edu>
2019-03-21 16:56:53 -0700

    BOAC-1816: Makes pages wait for user data before finishing loading

--- src/mixins/Loading.vue
+++ src/mixins/Loading.vue
@@ -5,12 +5,20 @@ import { mapActions, mapGetters } from 'vuex';
 export default {
   name: 'Loading',
   computed: {
-    ...mapGetters('context', ['loading'])
+    ...mapGetters('context', ['loading']),
+    ...mapGetters('user', ['user'])
   },
   beforeCreate: () => store.dispatch('context/loadingStart'),
   methods: {
     ...mapActions('context', ['loadingStart']),
     loaded() {
+      if (this.user) {
+        this.finishLoading();
+      } else {
+        this.$watch('user', this.finishLoading);
+      }
+    },
+    finishLoading() {
       store.dispatch('context/loadingComplete');
       this.$nextTick(() => {
         if (this.$refs.pageHeader) {
