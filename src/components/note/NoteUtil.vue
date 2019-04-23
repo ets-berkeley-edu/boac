20b313744cc224d0ce964e1bea8ce7b35ac81f1c
Paul Kerschen <kerschen@berkeley.edu>
2019-04-23 11:27:03 -0700

    BOAC-2112 Resize note attachment dropzone; no drops outside zone

---
+++ src/components/note/NoteUtil.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+  name: 'NoteUtil',
+  methods: {
+    initFileDropPrevention() {
+      window.addEventListener('dragenter', this.preventFileDropOutsideFormControl, false);
+      window.addEventListener('dragover', this.preventFileDropOutsideFormControl);
+      window.addEventListener('drop', this.preventFileDropOutsideFormControl);
+    },
+    preventFileDropOutsideFormControl(e) {
+      if (!e.target.classList.contains('form-control-file')) {
+        e.preventDefault();
+        e.dataTransfer.effectAllowed = 'none';
+        e.dataTransfer.dropEffect = 'none';
+      }
+    }
+  }
+}
+</script>
