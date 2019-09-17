c6bba52a4c640874f1809c926290a01a6428f90b
Paul Kerschen <kerschen@berkeley.edu>
2019-09-17 10:44:24 -0700

    BOAC-2616 Manually added users get default Canvas data privileges and persist through nightly refresh

--- scripts/db/tools/add_admin_user.sql
+++ scripts/db/tools/add_admin_user.sql
@@ -12,8 +12,9 @@ INSERT INTO authorized_users (uid, is_admin, created_at, updated_at)
   SELECT :'uid', true, now(), now()
   WHERE NOT EXISTS (SELECT id FROM authorized_users WHERE uid = :'uid');
 
--- Let us be sure that deleted status is NULL.
+-- If the UID represents an existing user, ensure deleted_at is null and can_access_canvas_data is true.
 UPDATE authorized_users SET deleted_at = NULL WHERE uid = :'uid';
+UPDATE authorized_users SET can_access_canvas_data = true WHERE uid = :'uid';
 
 -- Done
 
