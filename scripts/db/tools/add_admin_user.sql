1f64d540127816b574af82bbff1d3aeb0407326d
John Crossman <johncrossman@berkeley.edu>
2019-09-25 17:24:20 -0700

    BOAC-2775, add_user scripts need created_by value

--- scripts/db/tools/add_admin_user.sql
+++ scripts/db/tools/add_admin_user.sql
@@ -4,12 +4,12 @@ BEGIN;
 -- Register UID as BOA admin user
 --
 -- USAGE:
---   psql ... -v uid=123456 -f scripts/db/tools/add_admin_user.sql
+--   psql ... -v uid=123456 -v created_by=987654 -f scripts/db/tools/add_admin_user.sql
 --
 
 -- Create admin user
-INSERT INTO authorized_users (uid, is_admin, created_at, updated_at)
-  SELECT :'uid', true, now(), now()
+INSERT INTO authorized_users (uid, created_by, is_admin, created_at, updated_at)
+  SELECT :'uid', :'created_by', true, now(), now()
   WHERE NOT EXISTS (SELECT id FROM authorized_users WHERE uid = :'uid');
 
 -- If the UID represents an existing user, ensure deleted_at is null and can_access_canvas_data is true.
