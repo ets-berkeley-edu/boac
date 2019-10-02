a81131ccd38af027d09bb3ef8661d769ba5e6eb6
Paul Kerschen <kerschen@berkeley.edu>
2019-10-02 15:22:00 -0700

    BOAC-2800 Guest is not Other, Other is not Guest

--- boac/lib/berkeley.py
+++ boac/lib/berkeley.py
@@ -391,10 +391,7 @@ BERKELEY_DEPT_CODE_TO_PROGRAM_AFFILIATIONS = {
         # ADVD ('Advisor Delegate') and minor advisors get filed with major advisors.
         'affiliations': ['ADVD', 'MAJ', 'MIN'],
     },
-    # Our 'Guest' and catchall 'Other' departments get stuck with empty program codes.
-    'GUEST': {
-        'program': '',
-    },
+    # Our catchall 'Other' department gets stuck with empty program codes.
     'ZZZZZ': {
         'program': '',
     },
