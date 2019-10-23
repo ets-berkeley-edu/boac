24ac393ce8b0eebd0882fffe26343849d4cc09b3
John Crossman <johncrossman@berkeley.edu>
2019-10-23 10:56:04 -0700

    BOAC-2910, use aria-hidden on Cancel Appt; sr-only for full phrase

--- src/components/appointment/DropInWaitlist.vue
+++ src/components/appointment/DropInWaitlist.vue
@@ -124,7 +124,12 @@
               variant="outline-dark"
               @click="launchCheckInForAppointment(appointment)">
               <b-dropdown-item-button :id="`btn-appointment-${appointment.id}-details`" @click="showAppointmentDetails(appointment)">Details</b-dropdown-item-button>
-              <b-dropdown-item-button :id="`btn-appointment-${appointment.id}-cancel`" @click="cancelAppointment(appointment)">Cancel Appt</b-dropdown-item-button>
+              <b-dropdown-item-button
+                :id="`btn-appointment-${appointment.id}-cancel`"
+                @click="cancelAppointment(appointment)">
+                <span aria-hidden="true">Cancel Appt</span>
+                <span class="sr-only">Cancel Appointment</span>
+              </b-dropdown-item-button>
             </b-dropdown>
             <div
               v-if="isNil(appointment.checkedInAt) && !isNil(appointment.canceledAt)"
