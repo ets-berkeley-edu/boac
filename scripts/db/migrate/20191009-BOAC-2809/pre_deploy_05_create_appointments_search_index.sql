6fd89ec29581dcf646339b5ac98e714d54de1934
Anne Lyttle <alyttle@berkeley.edu>
2019-10-28 16:32:44 -0700

    BOAC-2894: Fixes sql bug in appointments search index

--- scripts/db/migrate/20191009-BOAC-2809/pre_deploy_05_create_appointments_search_index.sql
+++ scripts/db/migrate/20191009-BOAC-2809/pre_deploy_05_create_appointments_search_index.sql
@@ -3,8 +3,9 @@ BEGIN;
 DROP MATERIALIZED VIEW IF EXISTS appointments_fts_index;
 
 CREATE MATERIALIZED VIEW appointments_fts_index AS (
-  SELECT 
-    id, to_tsvector('english', trim(concat(details, ' ', cancel_reason, ' ', cancel_reason_explained))) AS fts_index
+  SELECT
+    id,
+    to_tsvector('english', details || ' ' || coalesce(cancel_reason, '') || ' ' || coalesce(cancel_reason_explained, '')) AS fts_index
   FROM appointments
   WHERE details IS NOT NULL
     AND deleted_at IS NULL
