919d0fd55afc04ef1e06f7293b27ecfb54750bc1
John Crossman <johncrossman@berkeley.edu>
2019-12-04 14:18:01 -0800

    NOJIRA, BOA v4.1 on master branch

--- boac/__init__.py
+++ boac/__init__.py
@@ -29,7 +29,7 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '4.0'
+__version__ = '4.1'
 
 db = SQLAlchemy()
 
