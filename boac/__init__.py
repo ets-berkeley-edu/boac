807c274d8148aaa4bc5efbcd6c881b5da0fb4739
Paul Kerschen <kerschen@berkeley.edu>
2020-03-12 10:58:17 -0700

    NOJIRA BOA v4.5

--- boac/__init__.py
+++ boac/__init__.py
@@ -29,7 +29,7 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '4.4'
+__version__ = '4.5'
 
 db = SQLAlchemy()
 
