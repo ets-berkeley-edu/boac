19d51420d39b3635b33d5315ca8482cac21f88eb
John Crossman <johncrossman@berkeley.edu>
2020-08-20 22:24:42 -0700

    BOAC-3017, API for creating, editing topics/reasons

--- boac/api/topic_controller.py
+++ boac/api/topic_controller.py
@@ -22,15 +22,15 @@ SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
 "AS IS". REGENTS HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES,
 ENHANCEMENTS, OR MODIFICATIONS.
 """
-
-from boac.api.util import advising_data_access_required, scheduler_required
+from boac.api.errors import BadRequestError
+from boac.api.util import admin_required, advising_data_access_required, scheduler_required
 from boac.lib.http import tolerant_jsonify
 from boac.lib.util import to_bool_or_none
 from boac.models.topic import Topic
 from flask import current_app as app, request
 
 
-@app.route('/api/topics/all', methods=['GET'])
+@app.route('/api/topics/all')
 @advising_data_access_required
 def get_all_topics():
     include_deleted = to_bool_or_none(request.args.get('includeDeleted'))
@@ -38,7 +38,7 @@ def get_all_topics():
     return tolerant_jsonify(_to_sorted_json(topics))
 
 
-@app.route('/api/topics/for_appointments', methods=['GET'])
+@app.route('/api/topics/for_appointments')
 @scheduler_required
 def get_topics_for_appointment():
     include_deleted = to_bool_or_none(request.args.get('includeDeleted'))
@@ -46,12 +46,55 @@ def get_topics_for_appointment():
     return tolerant_jsonify(_to_sorted_json(topics))
 
 
-@app.route('/api/topics/for_notes', methods=['GET'])
+@app.route('/api/topics/for_notes')
 @advising_data_access_required
 def get_topics_for_notes():
     include_deleted = to_bool_or_none(request.args.get('includeDeleted'))
     topics = Topic.get_all(available_in_notes=True, include_deleted=include_deleted)
     return tolerant_jsonify(_to_sorted_json(topics))
+
+
+@app.route('/api/topic/create', methods=['POST'])
+@admin_required
+def create_topic():
+    params = request.json
+    topic = params.get('topic', '').strip()
+    available_in_notes = to_bool_or_none(params.get('availableInNotes')) or False
+    available_in_appointments = to_bool_or_none(params.get('availableInAppointments')) or False
+    if not topic or not (available_in_notes or available_in_appointments):
+        raise BadRequestError('Required parameters are missing.')
+    topic = Topic.create_topic(
+        topic,
+        available_in_notes=available_in_notes,
+        available_in_appointments=available_in_appointments,
+    )
+    return tolerant_jsonify(topic.to_api_json())
+
+
+@app.route('/api/topic/update', methods=['POST'])
+@admin_required
+def update_topic():
+    params = request.json
+    topic_id = params.get('id', '')
+    topic = params.get('topic', '').strip()
+    available_in_notes = to_bool_or_none(params.get('availableInNotes')) or False
+    available_in_appointments = to_bool_or_none(params.get('availableInAppointments')) or False
+    if not topic_id or not topic or not (available_in_notes or available_in_appointments):
+        raise BadRequestError('Required parameters are missing.')
+    topic = Topic.update_topic(
+        topic_id=topic_id,
+        topic=topic,
+        available_in_notes=available_in_notes,
+        available_in_appointments=available_in_appointments,
+    )
+    return tolerant_jsonify(topic.to_api_json())
+
+
+@app.route('/api/topic/delete/<topic_id>', methods=['DELETE'])
+@admin_required
+def delete_topic(topic_id):
+    Topic.delete(topic_id=topic_id)
+    return tolerant_jsonify({'message': f'Topic {topic_id} deleted'}), 200
 
 
 def _to_sorted_json(topics):
--- tests/test_api/test_topic_controller.py
+++ tests/test_api/test_topic_controller.py
@@ -23,8 +23,13 @@ SOFTWARE AND ACCOMPANYING DOCUMENTATION, IF ANY, PROVIDED HEREUNDER IS PROVIDED
 ENHANCEMENTS, OR MODIFICATIONS.
 """
 
+import json
+
+from boac.models.topic import Topic
+from sqlalchemy import and_
 from tests.util import override_config
 
+admin_uid = '2040'
 coe_drop_in_advisor_uid = '1133399'
 coe_advisor_no_advising_data_uid = '1022796'
 coe_scheduler_uid = '6972201'
@@ -55,7 +60,7 @@ class TestGetTopics:
         """Get all topics, including deleted."""
         fake_auth.login(coe_drop_in_advisor_uid)
         api_json = self._api_all_topics(client)
-        topics = _get_topics(api_json)
+        topics = _get_topic_labels(api_json)
         assert 'Topic for all, 1' in topics
         assert 'Topic for appointments, 2' in topics
         assert 'Topic for notes, 9' in topics
@@ -67,7 +72,7 @@ class TestGetTopics:
         """Get all note topic options, not including deleted."""
         fake_auth.login(coe_drop_in_advisor_uid)
         api_json = self._api_all_topics(client, include_deleted=True)
-        topics = _get_topics(api_json)
+        topics = _get_topic_labels(api_json)
         assert 'Topic for all, 1' in topics
         assert 'Topic for notes, 9' in topics
         assert 'Topic for appointments, 2' in topics
@@ -76,44 +81,158 @@ class TestGetTopics:
         assert topics[-1] == 'Other / Reason not listed'
 
 
-class TestTopicsForAppointment:
+class TestCreateTopic:
+
+    def test_not_authenticated(self, client):
+        """Denies anonymous access."""
+        _api_create_topic(client, topic='Foo', available_in_notes=True, expected_status_code=401)
+
+    def test_deny_non_admin_user(self, client, fake_auth):
+        """Denies access to non-admin user."""
+        fake_auth.login(l_s_college_advisor_uid)
+        _api_create_topic(client, topic='Foo', available_in_notes=True, expected_status_code=401)
+
+    def test_invalid_arguments(self, client, fake_auth):
+        """Fails due to invalid arguments."""
+        fake_auth.login(admin_uid)
+        _api_create_topic(
+            client,
+            topic='A valid topic',
+            available_in_appointments=False,
+            available_in_notes=False,
+            expected_status_code=400,
+        )
+
+    def test_create_topic(self, client, fake_auth):
+        """Admin user can create a topic."""
+        fake_auth.login(admin_uid)
+        topic = 'A valid topic'
+        api_json = _api_create_topic(client, topic=topic, available_in_appointments=True)
+        assert api_json['id']
+        assert api_json['topic'] == topic
+        assert api_json['availableInAppointments'] is True
+        assert api_json['availableInNotes'] is False
+        assert api_json['deletedAt'] is None
+
+
+class TestUpdateTopic:
 
     @classmethod
-    def _get_topics(cls, client, include_deleted=None, expected_status_code=200):
-        api_path = '/api/topics/for_appointments'
-        if include_deleted is not None:
-            api_path += f'?includeDeleted={str(include_deleted).lower()}'
-        response = client.get(api_path)
+    def _api_update_topic(
+            cls,
+            client,
+            topic_id,
+            topic,
+            available_in_appointments=False,
+            available_in_notes=False,
+            expected_status_code=200,
+    ):
+        data = {
+            'id': topic_id,
+            'topic': topic,
+            'availableInAppointments': available_in_appointments,
+            'availableInNotes': available_in_notes,
+        }
+        response = client.post(
+            '/api/topic/update',
+            data=json.dumps(data),
+            content_type='application/json',
+        )
         assert response.status_code == expected_status_code
         return response.json
 
+    def test_not_authenticated(self, client):
+        """Denies anonymous access."""
+        self._api_update_topic(
+            client,
+            topic_id=_get_sample_topic().id,
+            topic='Logging in to BOA is for nerds.',
+            available_in_notes=True,
+            expected_status_code=401,
+        )
+
+    def test_deny_non_admin_user(self, client, fake_auth):
+        """Denies access to non-admin user."""
+        fake_auth.login(admin_uid)
+        api_json = _api_create_topic(client, topic='I, me, mine.', available_in_appointments=True)
+
+        fake_auth.login(l_s_college_advisor_uid)
+        self._api_update_topic(
+            client,
+            topic_id=api_json['id'],
+            topic='Advisor in denial.',
+            available_in_notes=True,
+            expected_status_code=401,
+        )
+
+    def test_update_topic(self, client, fake_auth):
+        """Admin user can update a topic."""
+        fake_auth.login(admin_uid)
+        api_json = _api_create_topic(client, topic='Like a boss.', available_in_appointments=True)
+        updated_topic = 'It feels good to be a gangster.'
+        api_json = self._api_update_topic(
+            client,
+            topic_id=api_json['id'],
+            topic=updated_topic,
+            available_in_notes=True,
+        )
+        assert api_json['topic'] == updated_topic
+
+
+class TestDeleteTopic:
+
+    def test_not_authenticated(self, client):
+        """Denies anonymous access."""
+        assert client.delete(f'/api/topic/delete/{_get_sample_topic().id}').status_code == 401
+
+    def test_deny_non_admin_user(self, client, fake_auth):
+        """Denies access to non-admin user."""
+        fake_auth.login(l_s_college_advisor_uid)
+        assert client.delete(f'/api/topic/delete/{_get_sample_topic().id}').status_code == 401
+
+    def test_delete_topic(self, client, fake_auth):
+        """Admin users can delete topics."""
+        fake_auth.login(admin_uid)
+        topic_label = 'Delete me, Seymour!'
+        topic = _api_create_topic(client, topic_label, available_in_appointments=True)
+        topic_id = topic['id']
+        assert topic['deletedAt'] is None
+        assert client.delete(f'/api/topic/delete/{topic_id}').status_code == 200
+        api_json = _topics_for_appointments(client, include_deleted=True)
+        deleted_topic = next((row for row in api_json if row['id'] == topic_id), None)
+        assert deleted_topic
+        assert deleted_topic['topic'] == topic_label
+
+
+class TestTopicsForAppointment:
+
     def test_mark_read_not_authenticated(self, client):
         """Returns 401 if not authenticated."""
-        self._get_topics(client, expected_status_code=401)
+        _topics_for_appointments(client, expected_status_code=401)
 
     def test_user_without_advising_data_access(self, app, client, fake_auth):
         """Denies access to a user who cannot access notes and appointments."""
         with override_config(app, 'DEPARTMENTS_SUPPORTING_DROP_INS', ['COENG']):
             fake_auth.login(coe_advisor_no_advising_data_uid)
-            self._get_topics(client, expected_status_code=401)
+            _topics_for_appointments(client, expected_status_code=401)
 
     def test_deny_advisor(self, app, client, fake_auth):
         """Returns 401 if user is not a drop-in advisor."""
         with override_config(app, 'DEPARTMENTS_SUPPORTING_DROP_INS', ['QCADV']):
             fake_auth.login(l_s_college_advisor_uid)
-            self._get_topics(client, expected_status_code=401)
+            _topics_for_appointments(client, expected_status_code=401)
 
     def test_dept_not_drop_in_enabled(self, client, fake_auth):
         """Returns 401 if user's dept is not configured for drop-in advising."""
         fake_auth.login(coe_scheduler_uid)
-        self._get_topics(client, expected_status_code=401)
+        _topics_for_appointments(client, expected_status_code=401)
 
     def test_scheduler_get_appointment_topics(self, app, client, fake_auth):
         """COE scheduler can get topics."""
         with override_config(app, 'DEPARTMENTS_SUPPORTING_DROP_INS', ['COENG']):
             fake_auth.login(coe_scheduler_uid)
-            api_json = self._get_topics(client)
-            topics = _get_topics(api_json)
+            api_json = _topics_for_appointments(client)
+            topics = _get_topic_labels(api_json)
             assert len(topics) == 9
             assert topics[-1] == 'Other / Reason not listed'
 
@@ -121,8 +240,8 @@ class TestTopicsForAppointment:
         """COE advisor can get topics."""
         with override_config(app, 'DEPARTMENTS_SUPPORTING_DROP_INS', ['COENG']):
             fake_auth.login(coe_drop_in_advisor_uid)
-            api_json = self._get_topics(client)
-            topics = _get_topics(api_json)
+            api_json = _topics_for_appointments(client)
+            topics = _get_topic_labels(api_json)
             assert len(topics) == 9
             assert topics[-1] == 'Other / Reason not listed'
 
@@ -130,11 +249,11 @@ class TestTopicsForAppointment:
         """Get all appointment topic options, including deleted."""
         with override_config(app, 'DEPARTMENTS_SUPPORTING_DROP_INS', ['COENG']):
             fake_auth.login(coe_drop_in_advisor_uid)
-            api_json = self._get_topics(client, include_deleted=True)
+            api_json = _topics_for_appointments(client, include_deleted=True)
             deleted_items = list(filter(lambda row: row['deletedAt'], api_json))
-            assert 'Topic for appointments, deleted' in _get_topics(deleted_items)
+            assert 'Topic for appointments, deleted' in _get_topic_labels(deleted_items)
             # Verify custom sort
-            assert _get_topics(api_json)[-1] == 'Other / Reason not listed'
+            assert _get_topic_labels(api_json)[-1] == 'Other / Reason not listed'
 
 
 class TestTopicsForNotes:
@@ -161,7 +280,7 @@ class TestTopicsForNotes:
         """Get all note topic options, including deleted."""
         fake_auth.login(coe_drop_in_advisor_uid)
         api_json = self._api_all_note_topics(client, include_deleted=True)
-        topics = _get_topics(api_json)
+        topics = _get_topic_labels(api_json)
         assert 'Topic for all, 1' in topics
         assert 'Topic for notes, 9' in topics
         assert 'Topic for notes, deleted' in topics
@@ -171,12 +290,47 @@ class TestTopicsForNotes:
         """Get all note topic options, not including deleted."""
         fake_auth.login(coe_drop_in_advisor_uid)
         api_json = self._api_all_note_topics(client)
-        topics = _get_topics(api_json)
+        topics = _get_topic_labels(api_json)
         assert 'Topic for all, 1' in topics
         assert 'Topic for notes, 9' in topics
         assert 'Topic for notes, deleted' not in topics
         assert topics[-1] == 'Other / Reason not listed'
 
 
-def _get_topics(api_json):
+def _api_create_topic(
+        client,
+        topic,
+        available_in_appointments=False,
+        available_in_notes=False,
+        expected_status_code=200,
+):
+    data = {
+        'topic': topic,
+        'availableInAppointments': available_in_appointments,
+        'availableInNotes': available_in_notes,
+    }
+    response = client.post(
+        '/api/topic/create',
+        data=json.dumps(data),
+        content_type='application/json',
+    )
+    assert response.status_code == expected_status_code
+    return response.json
+
+
+def _get_sample_topic():
+    topic = Topic.query.filter(and_(Topic.id == 1, Topic.deleted_at == None)).first()  # noqa: E711
+    assert topic
+    return topic
+
+
+def _get_topic_labels(api_json):
     return [row['topic'] for row in api_json]
+
+
+def _topics_for_appointments(client, include_deleted=None, expected_status_code=200):
+    api_path = '/api/topics/for_appointments'
+    api_path += f'?includeDeleted={str(include_deleted).lower()}' if include_deleted else ''
+    response = client.get(api_path)
+    assert response.status_code == expected_status_code
+    return response.json
