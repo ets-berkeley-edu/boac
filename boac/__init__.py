4611cd06311aee2c73b5d3b2143b59985649623d
John Crossman <johncrossman@berkeley.edu>
2020-08-26 10:31:28 -0700

    NOJIRA, master branch is now v4.10

--- boac/__init__.py
+++ boac/__init__.py
@@ -29,7 +29,7 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '4.9'
+__version__ = '4.10'
 
 db = SQLAlchemy()
 
