e471882f6abbbe449500930667d25e63b5b63954
John Crossman <johncrossman@berkeley.edu>
2020-10-21 16:12:39 -0700

    BOAC-3713, replace 'b-form-select' with more accessible 'b-dropdown', styled

--- src/components/student/SortBy.vue
+++ src/components/student/SortBy.vue
@@ -1,20 +1,50 @@
 <template>
-  <div class="align-items-center d-flex pb-1">
+  <div v-if="isReady" class="align-items-center d-flex pb-1">
     <div>
-      <label id="sort-by" class="mb-0 text-nowrap" for="students-sort-by">
-        <span class="sr-only">Use space-bar to open</span> Sort<span class="sr-only"> students</span> by
+      <label id="sort-by" class="mb-0 pr-2 text-nowrap" for="students-sort-by">
+        Sort<span class="sr-only"> students</span> by
       </label>
     </div>
-    <div class="pl-2">
-      <b-form-select
+    <div class="dropdown">
+      <b-dropdown
         id="students-sort-by"
-        v-model="selected"
         aria-labelledby="sort-by"
-        class="form-control px-3 py-1 w-auto"
-        :options="optionGroups"
-        text-field="name"
-        @change="onChange"
-      ></b-form-select>
+        ar
+        block
+        no-caret
+        right
+        toggle-class="dd-override"
+        variant="link"
+        @hidden="alertScreenReader('Sort-by menu closed')"
+        @shown="alertScreenReader('Sort-by menu opened')"
+      >
+        <template slot="button-content">
+          <div class="d-flex dropdown-width justify-content-between text-dark">
+            <div v-if="dropdownLabel">
+              <span class="sr-only">Students sorted by </span>{{ dropdownLabel }}<span class="sr-only">. Hit enter to open menu</span>
+            </div>
+            <div v-if="!dropdownLabel">Select...</div>
+            <div class="ml-2">
+              <font-awesome icon="angle-down" class="menu-caret" />
+            </div>
+          </div>
+        </template>
+        <b-dropdown-group
+          v-for="(group, gIndex) in optionGroups"
+          :id="`sort-by-option-group-${group.label}`"
+          :key="`group-${gIndex}`"
+          :header="group.label"
+        >
+          <b-dropdown-item-button
+            v-for="(option, index) in group.options"
+            :id="`sort-by-option-${option.value}`"
+            :key="`group-option-${index}`"
+            @click="onSelect(option.value)"
+          >
+            <span :class="{'font-weight-bolder': option.value === sortBy}">{{ option.label }}</span>
+          </b-dropdown-item-button>
+        </b-dropdown-group>
+      </b-dropdown>
     </div>
   </div>
 </template>
@@ -36,78 +66,118 @@ export default {
     }
   },
   data: () => ({
+    dropdownLabel: undefined,
+    isReady: false,
     optionGroups: undefined,
-    selected: undefined,
+    sortBy: undefined,
     sortByKey: undefined
   }),
   created() {
+    this.optionGroups = this.getSortByOptionGroups(this.domain)
     this.sortByKey = this.domain === 'admitted_students' ? 'admitSortBy' : 'sortBy'
-    this.$eventHub.$on(`${this.sortByKey}-user-preference-change`, sortBy => this.selected = sortBy)
-    this.selected = this.get(this.preferences, this.sortByKey)
-    if (this.domain === 'admitted_students') {
-      this.options = [
-        {name: 'First Name', value: 'first_name'},
-        {name: 'Last Name', value: 'last_name'},
-        {name: 'CS ID', value: 'cs_empl_id'}
-      ]
-    } else {
-      const previousTermId = this.previousSisTermId(this.$config.currentEnrollmentTermId)
-      const previousPreviousTermId = this.previousSisTermId(previousTermId)
-      const options = [
-        {name: 'First Name', value: 'first_name'},
-        {name: 'Last Name', value: 'last_name'},
-        {name: 'Level', value: 'level'},
-        {name: 'Major', value: 'major'},
-        {name: 'Entering Term', value: 'entering_term'}
-      ]
-      if (this.$currentUser.isAdmin || this.includes(this.myDeptCodes(['advisor', 'director']), 'UWASC')) {
-        options.push({name: 'Team', value: 'group_name'})
-      }
-      this.optionGroups = [
-        {
-          label: 'Profile',
-          options
-        },
-        {
-          label: 'Terms',
-          options: [
-            {html: 'Terms in Attendance, ascending', value: 'terms_in_attendance'},
-            {html: 'Terms in Attendance, descending', value: 'terms_in_attendance desc'}
-          ]
-        },
-        {
-          label: 'GPA',
-          options: [
-            {html: `${this.termNameForSisId(previousPreviousTermId)}, ascending`, value: `term_gpa_${previousPreviousTermId}`},
-            {html: `${this.termNameForSisId(previousPreviousTermId)}, descending`, value: `term_gpa_${previousPreviousTermId} desc`},
-            {html: `${this.termNameForSisId(previousTermId)}, ascending`, value: `term_gpa_${previousTermId}`},
-            {html: `${this.termNameForSisId(previousTermId)}, descending`, value: `term_gpa_${previousTermId} desc`},
-            {html: 'Cumulative, ascending', value: 'gpa'},
-            {html: 'Cumulative, descending', value: 'gpa desc'}
-          ]
-        },
-        {
-          label: 'Units',
-          options: [
-            {html: 'In Progress, ascending', value: 'enrolled_units'},
-            {html: 'In Progress, descending', value: 'enrolled_units desc'},
-            {html: 'Completed, ascending', value: 'units'},
-            {html: 'Completed, descending', value: 'units desc'}
-          ]
-        }
-      ]
-    }
+    this.$eventHub.$on(`${this.sortByKey}-user-preference-change`, v => this.sortBy = v)
+    this.sortBy = this.get(this.preferences, this.sortByKey)
+    this.dropdownLabel = this.getSortByOptionLabel(this.optionGroups, this.sortBy)
+    this.isReady = true
   },
   methods: {
-    onChange() {
-      if (this.selected !== this.get(this.preferences, this.sortByKey)) {
-        this.alertScreenReader(`Sorting by ${this.selected}`)
+    getSortByOptionGroups(domain) {
+      const optionGroups = []
+      if (domain === 'admitted_students') {
+        optionGroups.push({
+          label: 'Profile',
+          options: [
+            {label: 'First Name', value: 'first_name'},
+            {label: 'Last Name', value: 'last_name'},
+            {label: 'CS ID', value: 'cs_empl_id'}
+          ]
+        })
+      } else {
+        const previousTermId = this.previousSisTermId(this.$config.currentEnrollmentTermId)
+        const previousPreviousTermId = this.previousSisTermId(previousTermId)
+        optionGroups.push({
+          label: 'Profile',
+          options: [
+            {label: 'First Name', value: 'first_name'},
+            {label: 'Last Name', value: 'last_name'},
+            {label: 'Level', value: 'level'},
+            {label: 'Major', value: 'major'},
+            {label: 'Entering Term', value: 'entering_term'}
+          ]
+        })
+        if (this.$currentUser.isAdmin || this.includes(this.myDeptCodes(['advisor', 'director']), 'UWASC')) {
+          optionGroups[0].options.push({label: 'Team', value: 'group_name'})
+        }
+        optionGroups.push({
+          label: 'Terms',
+          options: [
+            {label: 'Terms in Attendance, ascending', value: 'terms_in_attendance'},
+            {label: 'Terms in Attendance, descending', value: 'terms_in_attendance desc'}
+          ]
+        })
+        optionGroups.push({
+          label: 'GPA',
+          options: [
+            {label: `${this.termNameForSisId(previousPreviousTermId)}, ascending`, value: `term_gpa_${previousPreviousTermId}`},
+            {label: `${this.termNameForSisId(previousPreviousTermId)}, descending`, value: `term_gpa_${previousPreviousTermId} desc`},
+            {label: `${this.termNameForSisId(previousTermId)}, ascending`, value: `term_gpa_${previousTermId}`},
+            {label: `${this.termNameForSisId(previousTermId)}, descending`, value: `term_gpa_${previousTermId} desc`},
+            {label: 'Cumulative, ascending', value: 'gpa'},
+            {label: 'Cumulative, descending', value: 'gpa desc'}
+          ]
+        })
+        optionGroups.push({
+          label: 'Units',
+          options: [
+            {label: 'In Progress, ascending', value: 'enrolled_units'},
+            {label: 'In Progress, descending', value: 'enrolled_units desc'},
+            {label: 'Completed, ascending', value: 'units'},
+            {label: 'Completed, descending', value: 'units desc'}
+          ]
+        })
+      }
+      return optionGroups
+    },
+    getSortByOptionLabel(optionGroups, value) {
+      let label = undefined
+      if (this.optionGroups) {
+        this.$_.each(optionGroups, group => {
+          this.$_.each(group.options, option => {
+            if (value === option.value) {
+              label = option.label
+            }
+            return !label
+          })
+          return !label
+        })
+      }
+      return label
+    },
+    onSelect(value) {
+      if (value !== this.get(this.preferences, this.sortByKey)) {
+        this.sortBy = value
+        this.dropdownLabel = this.getSortByOptionLabel(this.optionGroups, this.sortBy)
+        this.alertScreenReader(`${this.dropdownLabel} selected`)
         store.commit('currentUserExtras/setUserPreference', {
           key: this.sortByKey,
-          value: this.selected
+          value: this.sortBy
         })
       }
     }
   }
 }
 </script>
+
+<style scoped>
+.dropdown {
+  background-color: #fefefe;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  color: #000;
+  height: 42px;
+  text-align: left;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: 280px;
+}
+</style>
