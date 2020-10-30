cbfecd3b12314e4e3c0338f0b79d1ff8186a70ce
John Crossman <johncrossman@berkeley.edu>
2020-10-30 12:02:46 -0700

    BOAC-3678, screen-reader users can dismiss/restore BOA service announcement

--- src/store/modules/context.ts
+++ src/store/modules/context.ts
@@ -2,28 +2,34 @@ import { getServiceAnnouncement } from '@/api/config'
 
 const state = {
   announcement: undefined,
-  hasUserDismissedFooterAlert: false,
+  dismissedFooterAlert: false,
+  dismissedServiceAnnouncement: false,
   loading: undefined
 }
 
 const getters = {
   announcement: (state: any): string => state.announcement,
-  hasUserDismissedFooterAlert: (): boolean => state.hasUserDismissedFooterAlert,
+  dismissedFooterAlert: (): boolean => state.dismissedFooterAlert,
+  dismissedServiceAnnouncement: (): boolean => state.dismissedServiceAnnouncement,
   loading: (state: any): boolean => state.loading
 }
 
 const mutations = {
-  dismissFooterAlert: (state: any) => state.hasUserDismissedFooterAlert = true,
+  dismissFooterAlert: (state: any) => state.dismissedFooterAlert = true,
+  dismissServiceAnnouncement: (state: any) => state.dismissedServiceAnnouncement = true,
   loadingComplete: (state: any) => (state.loading = false),
   loadingStart: (state: any) => (state.loading = true),
+  restoreServiceAnnouncement: (state: any) => state.dismissedServiceAnnouncement = false,
   storeAnnouncement: (state: any, data: any) => (state.announcement = data),
 }
 
 const actions = {
   dismissFooterAlert: ({ commit }) => commit('dismissFooterAlert'),
+  dismissServiceAnnouncement: ({ commit }) => commit('dismissServiceAnnouncement'),
   loadingComplete: ({ commit }) => commit('loadingComplete'),
   loadingStart: ({ commit }) => commit('loadingStart'),
-  loadServiceAnnouncement: ({ commit }) => new Promise(() => getServiceAnnouncement().then(data => commit('storeAnnouncement', data)))
+  loadServiceAnnouncement: ({ commit }) => new Promise(() => getServiceAnnouncement().then(data => commit('storeAnnouncement', data))),
+  restoreServiceAnnouncement: ({ commit }) => commit('restoreServiceAnnouncement')
 }
 
 export default {
