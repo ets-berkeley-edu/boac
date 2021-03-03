fa9b1b998686968a9c1424f69783cf6f44216e6b
Anne Lyttle <alyttle@berkeley.edu>
2021-03-03 15:49:08 -0800

    BOAC-3932: Adds degree checks list, populated with fake data

--- src/views/DegreeChecks.vue
+++ src/views/DegreeChecks.vue
@@ -1,6 +1,86 @@
 <template>
-  <div>
+  <div class="ml-3 mr-3 mt-3">
     <Spinner />
+    <div v-if="!loading">
+      <h1 class="page-section-header pl-1">
+        Managing Degree Checks
+      </h1>
+      <router-link
+        id="degree-check-create-link"
+        class="d-flex flex-row-reverse justify-content-end pb-3"
+        to="degree/new"
+      >
+        Create new degree check
+        <font-awesome icon="plus" class="m-1" />
+      </router-link>
+      <b-table-lite
+        id="degree-checks-table"
+        :fields="[
+          {key: 'name', label: 'Degree Check', tdClass: 'align-middle'},
+          {key: 'createdAt', label: 'Created', tdClass: 'align-middle'},
+          {key: 'actions', label: '', thClass: 'w-40'}
+        ]"
+        :items="degreeChecks"
+        borderless
+        fixed
+        hover
+        responsive
+        small
+        stacked="md"
+        striped
+        thead-class="sortable-table-header text-nowrap"
+      >
+        <template #cell(name)="row">
+          <router-link
+            :id="`degree-check-${row.index}-link`"
+            :to="`degree/${row.item.id}`"
+            v-html="`${row.item.name}`"
+          ></router-link>
+        </template>
+        <template #cell(createdAt)="row">
+          {{ row.item.createdAt | moment('MMM D, YYYY') }}
+        </template>
+        <template #cell(actions)="row">
+          <div class="d-flex flex-nowrap">
+            <b-btn
+              :id="`degree-check-${row.index}-print-btn`"
+              class="p-1"
+              variant="link"
+              @click.stop="print(row.item.id)"
+            >
+              Print
+            </b-btn>
+            <span class="separator">|</span>
+            <b-btn
+              :id="`degree-check-${row.index}-rename-btn`"
+              class="p-1"
+              variant="link"
+              @click.stop="rename(row.item.id)"
+            >
+              Rename
+            </b-btn>
+            <span class="separator">|</span>
+            <b-btn
+              :id="`degree-check-${row.index}-copy-btn`"
+              class="p-1"
+              variant="link"
+              @click.stop="copy(row.item.id)"
+            >
+              Copy
+            </b-btn>
+            <span class="separator">|</span>
+            <b-btn
+              :id="`degree-check-${row.index}-delete-btn`"
+              class="p-1"
+              variant="link"
+              @click.stop="del(row.item.id)"
+            >
+              Delete
+            </b-btn>
+          </div>
+        </template>
+      </b-table-lite>
+    </div>
   </div>
 </template>
 
@@ -14,13 +94,26 @@ export default {
   name: 'DegreeChecks',
   components: {Spinner},
   mixins: [Context, Loading, Util],
-  created() {
+  data: () => ({
+    degreeChecks: [
+      {name: 'Bioengineering 2019', createdAt: '2019-07-18', id: 0},
+      {name: 'Bioengineering 2020', createdAt: '2019-07-18', id: 1},
+      {name: 'Bioengineering 2021', createdAt: '2019-07-18', id: 2},
+    ]
+  }),
+  mounted() {
     if (this.$config.featureFlagDegreeCheck) {
       // TODO: make an API call (BOAC-3935)
-      this.$_.noop()
+      this.loaded('Degree Checks loaded')
     } else {
       this.$router.push({path: '/404'})
     }
   },
 }
 </script>
+
+<style scoped>
+.separator {
+  color: #ccc;
+}
+</style>
