30fb1d843b42d1846772d393edc5e55c96c9ad46
John Crossman <johncrossman@berkeley.edu>
2021-04-27 09:00:41 -0700

    Revert "BOAC-4117, in degree_progress_category_type (db enum) rename 'Course' to 'CourseRequirement'"
    This reverts commit b30ae4efce67aabc2375bacb8f0a6ee7e9382816.

--- scripts/db/migrate/2021/20210426-BOAC-4117/pre_deploy_01_change_degree_progress_category_types.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
-UPDATE pg_enum SET enumlabel = 'Course Requirement'
-WHERE enumlabel = 'Course' AND enumtypid = (
-  SELECT oid FROM pg_type WHERE typname = 'degree_progress_category_types'
-);
-
-COMMIT;
