eaf331b668a23d649134f7fe257bb4cc4c41fafa
Anne Lyttle <alyttle@berkeley.edu>
2021-05-05 14:21:18 -0700

    BOAC-4033: Removes failing UPDATE from SQL migration

--- scripts/db/migrate/2021/20210416-BOAC-4033/pre_deploy_01_degree_progress_alter_category_course_units.sql
+++ scripts/db/migrate/2021/20210416-BOAC-4033/pre_deploy_01_degree_progress_alter_category_course_units.sql
@@ -1,12 +1,5 @@
 BEGIN;
 
-UPDATE degree_progress_categories
-SET course_units = FORMAT('%s,%s', 
-    LEAST(LEFT(course_units,1), RIGHT(course_units,1)),
-    GREATEST(LEFT(course_units,1), RIGHT(course_units,1))
-)
-WHERE course_units IS NOT NULL;
-
 ALTER TABLE degree_progress_categories ALTER COLUMN course_units TYPE int4range USING NULLIF(FORMAT('[%s]', course_units), '[]')::int4range;
 
 COMMIT;
