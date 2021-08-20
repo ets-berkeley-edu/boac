113c06c4733f18337927342f573ab967eb251d61
John Crossman <johncrossman@berkeley.edu>
2021-08-20 14:37:31 -0700

    BOAC-4392, URLs in category.description are linked in non-printable view

--- src/components/degree/Category.vue
+++ src/components/degree/Category.vue
@@ -60,7 +60,13 @@
         class="pl-1 py-1"
         :class="{'font-size-12': printable}"
       >
-        <pre class="text-wrap">{{ category.description }}</pre>
+        <pre v-if="printable" class="text-wrap">{{ category.description }}</pre>
+        <span
+          v-if="!printable"
+          v-linkified
+          class="text-wrap"
+          v-html="category.description"
+        />
       </div>
     </div>
     <AreYouSureModal
