b181f340917e42a0bc1c95328f64aa87dc6dd649
Anne Lyttle <alyttle@berkeley.edu>
2021-10-15 12:00:32 -0700

    BOAC-4450: wires checkboxes to Campus Requirements

--- src/mixins/DegreeEditSession.vue
+++ src/mixins/DegreeEditSession.vue
@@ -63,6 +63,7 @@ export default {
       'setDisableButtons',
       'setDraggingTarget',
       'setIncludeNotesWhenPrint',
+      'toggleCampusRequirement',
       'updateCategory',
       'updateCourse',
       'updateCourseRequirement',
