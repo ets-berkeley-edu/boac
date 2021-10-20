1b589b6d02741255722dd6f1e44a6f1cdcb50697
Anne Lyttle <alyttle@berkeley.edu>
2021-10-20 14:07:47 -0700

    NOJIRA: Version bump to 5.2

--- boac/__init__.py
+++ boac/__init__.py
@@ -29,7 +29,7 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '5.1'
+__version__ = '5.2'
 
 db = SQLAlchemy()
 
