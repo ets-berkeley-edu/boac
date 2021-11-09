c323efa13c0493d2310d80697502a9e634d6ec7f
John Crossman <johncrossman@berkeley.edu>
2021-11-09 11:21:56 -0800

    BOAC-4528, will this playful tweak wake up the boa-dev pipeline?

--- src/views/FlightDataRecorder.vue
+++ src/views/FlightDataRecorder.vue
@@ -5,8 +5,8 @@
       <div class="d-flex justify-content-between">
         <div>
           <div class="align-items-center d-flex pb-0">
-            <div class="pr-3">
-              <font-awesome :style="{color: '#3b7ea5'}" icon="chart-line" size="2x" />
+            <div class="pr-2">
+              <font-awesome :style="{color: '#3b7ea5'}" icon="chart-pie" size="2x" />
             </div>
             <div class="pt-2">
               <h1 class="page-section-header">Flight Data Recorder</h1>
@@ -48,7 +48,7 @@ import Util from '@/mixins/Util'
 import {getAvailableDepartmentReports} from '@/api/reports'
 
 export default {
-  name: 'Admin',
+  name: 'FlightDataRecorder',
   components: {
     NotesReport,
     Spinner,
