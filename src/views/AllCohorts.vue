1c221a05053e299bee4b5bfc90dd408a8830fbb1
John Crossman <johncrossman@berkeley.edu>
2021-12-08 11:13:21 -0800

    BOAC-4573, 'Everyone's Cohorts|Groups' omits advisors with zero cohorts|groups

--- src/views/AllCohorts.vue
+++ src/views/AllCohorts.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="m-3">
+  <div class="m-4">
     <Spinner />
     <div v-if="!loading">
-      <h1 class="mb-4">
+      <h1 class="mb-4 page-section-header">
         Everyone's Cohorts
       </h1>
       <div v-if="!rows.length">
@@ -13,14 +13,11 @@
           <span v-if="row.user.name">{{ row.user.name }}</span>
           <span v-if="!row.user.name">UID: {{ row.user.uid }}</span>
         </h2>
-        <ul v-if="row.cohorts.length">
+        <ul>
           <li v-for="cohort in row.cohorts" :key="cohort.id">
             <router-link :to="'/cohort/' + cohort.id">{{ cohort.name }}</router-link> ({{ cohort.totalStudentCount }})
           </li>
         </ul>
-        <div v-if="!row.cohorts.length" class="m-3">
-          User has no cohorts.
-        </div>
       </div>
     </div>
   </div>
@@ -41,7 +38,7 @@ export default {
   }),
   created() {
     getUsersWithCohorts('default').then(data => {
-      this.rows = data
+      this.rows = this.$_.filter(data, row => row.cohorts.length)
       this.loaded('Everyone\'s Cohorts page has loaded')
     })
   }
