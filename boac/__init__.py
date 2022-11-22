afaccfd680bc5e000d4ceadfed94c72bdf0f7a0f
John Crossman <johncrossman@berkeley.edu>
2022-11-22 13:13:41 -0800

    NOJIRA, BOA is now v5.14 on dev branch

--- boac/__init__.py
+++ boac/__init__.py
@@ -29,7 +29,7 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '5.13'
+__version__ = '5.14'
 
 db = SQLAlchemy()
 
