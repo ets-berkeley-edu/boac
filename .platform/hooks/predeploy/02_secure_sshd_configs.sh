19982048e10cbfc81618b4818542a026700cc292
John Crossman <johncrossman@berkeley.edu>
2022-12-06 13:34:31 -0800

    [QA] NOJIRA, custom branch for new boac AWS env, new S3 bucket for configs

--- .platform/hooks/predeploy/02_secure_sshd_configs.sh
+++ .platform/hooks/predeploy/02_secure_sshd_configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PYTHONPATH='' aws s3 cp s3://la-deploy-configs/shared/security/CIS-OS-Security-Config-Benchmarks-1.0/sshd_config/sed_commands_default.v1 /tmp/sed_commands
+PYTHONPATH='' aws s3 cp s3://boac-deploy-configs/shared/security/CIS-OS-Security-Config-Benchmarks-1.0/sshd_config/sed_commands_default.v1 /tmp/sed_commands
 sudo chown root:root /etc/ssh/sshd_config
 sudo chmod og-rwx /etc/ssh/sshd_config
 sudo cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.default
