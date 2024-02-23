29139cf7dac70043a29cfd5b9d89c9f453c0384a
John Crossman <johncrossman@berkeley.edu>
2024-02-22 17:05:57 -0800

    BOAC-5084, migrate remaining bits of stores/context.ts

--- src/stores/context.ts
+++ src/stores/context.ts
@@ -1,19 +1,120 @@
+import _ from 'lodash'
 import mitt from 'mitt'
+import router from '@/router'
 import {defineStore} from 'pinia'
+import {nextTick} from 'vue'
 
 export const useContextStore = defineStore('context', {
   state: () => ({
+    announcement: undefined,
     config: undefined,
-    currentUser: {},
-    eventHub: mitt()
+    currentUser: undefined,
+    dismissedFooterAlert: false,
+    dismissedServiceAnnouncement: false,
+    eventHub: mitt(),
+    loading: undefined,
+    loadingStartTime: undefined,
+    screenReaderAlert: {
+      message: '',
+      politeness: 'polite'
+    }
   }),
   actions: {
-    setConfig(config: any) {
-      this.config = config
+    addMyCohort(cohort: any) {
+      this.currentUser.myCohorts.push(cohort)
     },
-    setCurrentUser(user: any) {
-      this.currentUser = user
-      this.eventHub.emit('current-user-update')
+    addMyCuratedGroup(curatedGroup: any) {
+      this.currentUser.myCuratedGroups.push(curatedGroup)
+      this.currentUser.myCuratedGroups = _.sortBy(this.currentUser.myCuratedGroups, 'name')
+    },
+    alertScreenReader(message: string, politeness?: string) {
+      this.setScreenReaderAlert = {message: ''}
+      nextTick(() => {
+        this.setScreenReaderAlert = {message, politeness}
+      }).then(_.noop)
+    },
+    broadcast(eventType, data) {
+      this.eventHub.emit(eventType, data)
+    },
+    dismissFooterAlert() {
+      this.dismissedFooterAlert = true
+    },
+    dismissServiceAnnouncement() {
+      this.dismissedServiceAnnouncement = true
+    },
+    loadingComplete(srAlert?: any) {
+      if (!this.config.isProduction) {
+        console.log(`Page loaded in ${(new Date().getTime() - this.loadingStartTime) / 1000} seconds`)
+      }
+      this.loading = false
+      this.alertScreenReader(srAlert || `${String(_.get(router.currentRoute, 'name', ''))} page loaded.`)
+
+      const callable = () => {
+        const elements = document.getElementsByTagName('h1')
+        if (elements.length > 0) {
+          elements[0].setAttribute('tabindex', '-1')
+          elements[0].focus()
+        }
+        return elements.length > 0
+      }
+      nextTick(() => {
+        let counter = 0
+        const job = setInterval(() => (callable() || ++counter > 3) && clearInterval(job), 500)
+      }).then(_.noop)
+    },
+    loadingStart(route?: string|Object) {
+      this.loading = true
+      this.loadingStartTime = new Date().getTime()
+      this.alertScreenReader(`${String(_.get(route, 'name', ''))} page is loading`)
+    },
+    removeEventHandler(type, handler) {
+      this.eventHub.off(type, handler)
+    },
+    removeMyCohort(cohortId: number) {
+      const indexOf = this.currentUser.myCohorts.findIndex(cohort => cohort.id === cohortId)
+      this.currentUser.myCohorts.splice(indexOf, 1)
+    },
+    removeMyCuratedGroup(curatedGroupId: number) {
+      const indexOf = this.currentUser.myCuratedGroups.findIndex(curatedGroup => curatedGroup.id === curatedGroupId)
+      this.currentUser.myCuratedGroups.splice(indexOf, 1)
+    },
+    restoreServiceAnnouncement() {
+      this.dismissedServiceAnnouncement = false
+    },
+    setConfig(data: any) {
+      this.config = data
+    },
+    setCurrentUser(currentUser: any) {
+      this.currentUser = currentUser
+    },
+    setDemoMode(inDemoMode: any): void {
+      this.currentUser.inDemoMode = inDemoMode
+    },
+    setEventHandler(type, handler) {
+      this.eventHub.on(type, handler)
+    },
+    setMyDraftNoteCount(count: number) {
+      this.currentUser.myDraftNoteCount = count
+    },
+    setScreenReaderAlert(screenReaderAlert: any) {
+      this.screenReaderAlert = {
+        message: screenReaderAlert.message,
+        politeness: screenReaderAlert.politeness || 'polite'
+      }
+    },
+    setServiceAnnouncement(data: any) {
+      this.announcement = data
+    },
+    updateCurrentUserPreference(key, value) {
+      this.currentUser.preferences[key] = value
+    },
+    updateMyCohort(updatedCohort: any) {
+      const cohort = this.currentUser.myCohorts.find(cohort => cohort.id === +updatedCohort.id)
+      Object.assign(cohort, updatedCohort)
+    },
+    updateMyCuratedGroup(updatedCuratedGroup: any) {
+      const group = this.currentUser.myCuratedGroups.find(group => group.id === +updatedCuratedGroup.id)
+      Object.assign(group, updatedCuratedGroup)
     }
   }
 })
