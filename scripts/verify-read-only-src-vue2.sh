8c0c2731cfb74b19860d334323f90b7c263017a3
John Crossman <johncrossman@berkeley.edu>
2024-02-23 14:46:05 -0800

    NO-JIRA, local lint-vue (in tox.ini) invokes ./scripts/verify-read-only-src-vue2.sh

--- scripts/verify-read-only-src-vue2.sh
+++ scripts/verify-read-only-src-vue2.sh
@@ -10,5 +10,7 @@ total_line_count=$(find ${PWD}/src-vue2 -name '*.*' | xargs wc -l | grep total)
 if [[ "${total_line_count}" == *"${expected_total_line_count}"* ]]; then
   exit 0
 else
+  echo; echo '[ERROR] Changes have been made in src-vue2 directory. That is a no-no.'
+  echo; echo
   exit 1
 fi
