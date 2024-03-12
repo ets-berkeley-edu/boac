a18b6d1de4236bed5b7302be65e55f743e960f96
John Crossman <johncrossman@berkeley.edu>
2024-03-12 16:15:26 -0700

    BOAC-5111, Vuetify components for adv search modal

--- src/components/search/AdvancedSearchCheckboxes.vue
+++ src/components/search/AdvancedSearchCheckboxes.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pb-2">
-    <div class="font-size-16 font-weight-700 search-form-label">
+    <div class="font-size-16 font-weight-bold">
       What is the extent of your search?
       <div
         class="font-size-14 font-weight-500 ml-2"
@@ -9,64 +9,56 @@
         Select one or more.
       </div>
     </div>
-    <div class="align-items-center d-flex flex-wrap ml-2">
-      <div v-if="domain.includes('admits')" class="align-items-center d-flex mr-3">
-        <v-checkbox
-          id="search-include-admits-checkbox"
-          v-model="includeAdmits"
-          class="z-index-0"
-        />
-        <label
-          for="search-include-admits-checkbox"
-          class="search-form-label"
-        >
+    <div class="align-center d-flex flex-wrap">
+      <v-checkbox
+        v-if="domain.includes('admits')"
+        id="search-include-admits-checkbox"
+        v-model="includeAdmits"
+        color="primary"
+        hide-details
+        label="Students"
+      >
+        <template #label>
           <span class="sr-only">Search for</span>
           Admitted Students
-        </label>
-      </div>
-      <div class="align-items-center d-flex mr-3">
-        <v-checkbox
-          id="search-include-students-checkbox"
-          v-model="includeStudents"
-          class="z-index-0"
-        />
-        <label
-          for="search-include-students-checkbox"
-          class="search-form-label"
-        >
+        </template>
+      </v-checkbox>
+      <v-checkbox
+        id="search-include-students-checkbox"
+        v-model="includeStudents"
+        color="primary"
+        hide-details
+      >
+        <template #label>
           <span class="sr-only">Search for</span>
           Students
-        </label>
-      </div>
-      <div v-if="domain.includes('courses')" class="align-items-center d-flex mr-3">
-        <v-checkbox
-          id="search-include-courses-checkbox"
-          v-model="includeCourses"
-          class="z-index-0"
-        />
-        <label
-          for="search-include-courses-checkbox"
-          class="search-form-label"
-        >
+        </template>
+      </v-checkbox>
+      <v-checkbox
+        v-if="domain.includes('courses')"
+        id="search-include-courses-checkbox"
+        v-model="includeCourses"
+        color="primary"
+        hide-details
+      >
+        <template #label>
           <span class="sr-only">Search for</span>
           Classes
-        </label>
-      </div>
-      <div v-if="currentUser.canAccessAdvisingData" class="align-items-center d-flex mr-3">
-        <v-checkbox
-          id="search-include-notes-checkbox"
-          v-model="includeNotes"
-          class="z-index-0"
-          @change="onChangeIncludeNotes"
-        />
-        <label
-          for="search-include-notes-checkbox"
-          class="search-form-label"
-        >
+        </template>
+      </v-checkbox>
+      <v-checkbox
+        v-if="currentUser.canAccessAdvisingData"
+        id="search-include-notes-checkbox"
+        v-model="includeNotes"
+        color="primary"
+        hide-details
+        @change="onChangeIncludeNotes"
+      >
+        <template #label>
           <span class="sr-only">Search for</span>
           Notes &amp; Appointments
-        </label>
-      </div>
+        </template>
+      </v-checkbox>
     </div>
   </div>
 </template>
@@ -92,9 +84,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.search-form-label {
-  margin-bottom: 2px;
-}
-</style>
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,18 +1,23 @@
+import 'v-calendar/style.css'
 import axios from '@/plugins/axios'
-import type {App} from 'vue'
+import BaseInput from 'v-calendar'
 import CKEditor from '@ckeditor/ckeditor5-vue'
+import defaultExport from 'vue3-shortkey'
+import type {App} from 'vue'
+import vuetify from './vuetify'
 import {createPinia} from 'pinia'
 import {FocusTrap} from 'focus-trap-vue'
-import {setupCalendar} from 'v-calendar'
-import vuetify from './vuetify'
-import defaultExport from 'vue3-shortkey'
+import {Calendar, DatePicker, setupCalendar} from 'v-calendar'
 
 export function registerPlugins (app: App) {
   app.use(axios, {baseUrl: import.meta.env.VITE_APP_API_BASE_URL})
     .use(CKEditor)
     .use(createPinia())
-    .component('FocusTrap', FocusTrap)
     .use(setupCalendar, {})
     .use(vuetify)
     .use(defaultExport)
+    .component('FocusTrap', FocusTrap)
+    .component('VBaseInput', BaseInput)
+    .component('VCalendar', Calendar)
+    .component('VDatePicker', DatePicker)
 }
