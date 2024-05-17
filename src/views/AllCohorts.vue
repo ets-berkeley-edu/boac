72e61a2742eae650e4bcfe41bcc279b6c9dbdef4
John Crossman <johncrossman@berkeley.edu>
2024-05-17 12:52:03 -0700

    BOAC-5267, Vue 3 for Everyone's Cohorts

--- src/views/AllCohorts.vue
+++ src/views/AllCohorts.vue
@@ -1,24 +1,28 @@
 <template>
   <div v-if="!loading" class="default-margins">
-    <div class="mb-4">
+    <div class="mb-8">
       <h1 class="page-section-header">Everyone's Cohorts</h1>
       <div v-if="includesAdmittedStudents" class="pl-1">
-        <v-icon aria-label="Star icon" class="accent-color-orange" :icon="mdiStar" />
+        <v-icon
+          aria-label="Star icon"
+          color="warning"
+          :icon="mdiStar"
+        />
         denotes a cohort of admitted students.
       </div>
     </div>
     <div v-if="!rows.length">
       <div>There are no saved cohorts</div>
     </div>
-    <div v-for="(row, index) in rows" :key="index">
+    <div v-for="(row, index) in rows" :key="index" class="mt-4">
       <h2 class="page-section-header-sub">
         <span v-if="row.user.name">{{ row.user.name }}</span>
         <span v-if="!row.user.name">UID: {{ row.user.uid }}</span>
       </h2>
       <ul>
-        <li v-for="cohort in row.cohorts" :key="cohort.id">
+        <li v-for="cohort in row.cohorts" :key="cohort.id" class="ml-8">
           <span v-if="cohort.domain === 'admitted_students'" class="mr-1 text-success">
-            <v-icon aria-label="Star icon" class="accent-color-orange" :icon="mdiStar" />
+            <v-icon aria-label="Star icon" color="warning" :icon="mdiStar" />
             <span class="sr-only">Admitted Students</span>
           </span>
           <router-link :to="'/cohort/' + cohort.id">{{ cohort.name }}</router-link> ({{ cohort.totalStudentCount }})
@@ -29,27 +33,19 @@
 </template>
 
 <script setup>
-import {mdiStar} from '@mdi/js'
-</script>
-
-<script>
-import Context from '@/mixins/Context'
-import Util from '@/mixins/Util'
+import {computed} from 'vue'
+import {filter as _filter, find, flatten, map} from 'lodash'
 import {getUsersWithCohorts} from '@/api/cohort'
+import {mdiStar} from '@mdi/js'
+import {useContextStore} from '@/stores/context'
 
-export default {
-  name: 'AllCohorts',
-  mixins: [Context, Util],
-  data: () => ({
-    rows: [],
-    includesAdmittedStudents: undefined,
-  }),
-  created() {
-    getUsersWithCohorts().then(data => {
-      this.rows = this._filter(data, row => row.cohorts.length)
-      this.includesAdmittedStudents = this._find(this._flatten(this._map(this.rows, 'cohorts')), g => g.domain === 'admitted_students')
-      this.loadingComplete('Everyone\'s Cohorts page has loaded')
-    })
-  }
-}
+let includesAdmittedStudents = undefined
+const loading = computed(() => useContextStore().loading)
+let rows = []
+
+getUsersWithCohorts().then(data => {
+  rows = _filter(data, row => row.cohorts.length)
+  includesAdmittedStudents = find(flatten(map(rows, 'cohorts')), g => g.domain === 'admitted_students')
+  useContextStore().loadingComplete('Everyone\'s Cohorts loaded')
+})
 </script>
