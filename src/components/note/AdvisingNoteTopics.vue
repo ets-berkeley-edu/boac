ee06d97b95ec7768dc7ab458878533bf304d9c50
John Crossman <johncrossman@berkeley.edu>
2024-06-07 15:03:51 -0700

    BOAC-5189, EditNoteModal fixes

--- src/components/note/AdvisingNoteTopics.vue
+++ src/components/note/AdvisingNoteTopics.vue
@@ -1,16 +1,17 @@
 <template>
   <div>
-    <label id="add-note-topic-label" class="font-weight-bold font-size-14" for="add-note-topic">
+    <label id="add-note-topic-label" class="font-size-16 font-weight-700" for="add-note-topic">
       Topic Categories
     </label>
     <v-select
+      v-if="topicOptions.length"
       id="add-topic-select-list"
-      :model-value="selectedTopics"
       class="mt-2"
       density="compact"
       :disabled="disabled"
       hide-details
       :items="topicOptions"
+      :model-value="selectedTopics"
       multiple
       persistent-hint
       single-line
@@ -22,12 +23,11 @@
           v-bind="props"
           class="min-height-unset py-1"
           density="compact"
-        >
-        </v-list-item>
+        />
       </template>
       <template #selection="{item, index}">
         <v-chip
-          :id="`${notePrefix}-topic-${index}`"
+          :id="`${noteId ? `note-${noteId}` : 'note'}-topic-${index}`"
           class="v-chip-content-override font-weight-bold text-medium-emphasis text-uppercase text-no-wrap"
           closable
           :close-label="`Remove ${item}`"
@@ -39,7 +39,7 @@
         >
           <span class="truncate-with-ellipsis">{{ item.title }}</span>
           <template #close>
-            <v-icon color="error" :icon="mdiCloseCircle"></v-icon>
+            <v-icon color="error" :icon="mdiCloseCircle" />
           </template>
         </v-chip>
       </template>
@@ -48,75 +48,57 @@
 </template>
 
 <script setup>
-import {mdiCloseCircle} from '@mdi/js'
-</script>
-
-<script>
+import {alertScreenReader, putFocusNextTick} from '@/lib/utils'
+import {computed} from 'vue'
 import {differenceBy, each, size} from 'lodash'
 import {getTopicsForNotes} from '@/api/topics'
-import {alertScreenReader, putFocusNextTick} from '@/lib/utils'
+import {mdiCloseCircle} from '@mdi/js'
 import {useNoteStore} from '@/stores/note-edit-session'
 
-export default {
-  name: 'AdvisingNoteTopics',
-  props: {
-    noteId: {
-      default: undefined,
-      type: Number,
-      required: false
-    }
-  },
-  data: () => ({
-    topicOptions: []
-  }),
-  computed: {
-    disabled() {
-      return !!(useNoteStore().isSaving || useNoteStore().boaSessionExpired)
-    },
-    notePrefix() {
-      return this.noteId ? `note-${this.noteId}` : 'note'
-    },
-    selectedTopics: {
-      get() {
-        return useNoteStore().model.topics
-      },
-      set(topics) {
-        const topicsToAdd = differenceBy(topics, useNoteStore().model.topics)
-        const topicsToRemove = differenceBy(useNoteStore().model.topics, topics)
-        if (size(topicsToAdd)) {
-          this.add(topicsToAdd[0])
-        } else if (size(topicsToRemove)) {
-          this.remove(topicsToRemove[0])
-        }
-      }
-    }
-  },
-  created() {
-    getTopicsForNotes(false).then(rows => {
-      each(rows, row => {
-        this.topicOptions.push(row.topic)
-      })
-    })
-  },
-  methods: {
-    add(topic) {
-      if (topic) {
-        useNoteStore().addTopic(topic)
-        putFocusNextTick('add-topic-select-list')
-        alertScreenReader(`Topic ${topic} added.`)
-      }
-    },
-    onClickRemove(topic) {
-      this.remove(topic)
-    },
-    onUpdate(topics) {
-      this.selectedTopics = topics
-    },
-    remove(topic) {
-      useNoteStore().removeTopic(topic)
-      alertScreenReader(`Removed topic ${topic}.`)
-      putFocusNextTick('add-topic-select-list')
-    }
+defineProps({
+  noteId: {
+    default: undefined,
+    type: Number,
+    required: false
+  }
+})
+
+const noteStore = useNoteStore()
+const disabled = computed(() => noteStore.isSaving || noteStore.boaSessionExpired)
+const topicOptions = []
+
+getTopicsForNotes(false).then(rows => each(rows, row => topicOptions.push(row.topic)))
+
+const add = topic => {
+  if (topic) {
+    noteStore.addTopic(topic)
+    putFocusNextTick('add-topic-select-list')
+    alertScreenReader(`Topic ${topic} added.`)
   }
 }
+
+const remove = topic => {
+  noteStore.removeTopic(topic)
+  alertScreenReader(`Removed topic ${topic}.`)
+  putFocusNextTick('add-topic-select-list')
+}
+
+const onClickRemove = topic => remove(topic)
+
+const selectedTopics = computed({
+  get() {
+    return noteStore.model.topics
+  },
+  set(topics) {
+    const topicsToAdd = differenceBy(topics, noteStore.model.topics)
+    const topicsToRemove = differenceBy(noteStore.model.topics, topics)
+    if (size(topicsToAdd)) {
+      add(topicsToAdd[0])
+    } else if (size(topicsToRemove)) {
+      remove(topicsToRemove[0])
+    }
+  }
+})
+
+const onUpdate = topics => selectedTopics.value = topics
 </script>
