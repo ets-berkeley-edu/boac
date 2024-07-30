c43bc04e5bc29f83046034b5399c827788c0f1b4
John Crossman <johncrossman@berkeley.edu>
2024-07-30 14:37:57 -0700

    BOAC-5367, /degree Unit Reqts: Aww CRUD

--- src/components/degree/UnitsInput.vue
+++ src/components/degree/UnitsInput.vue
@@ -1,169 +1,153 @@
 <template>
   <div>
-    <div class="d-flex">
+    <div class="align-center d-flex">
       <label id="units-input-label" :class="labelClass" :for="inputId">
         {{ label }}
       </label>
       <div v-if="range" class="font-size-12">
         [<v-btn
           id="show-upper-units-input"
-          class="toggle-btn"
-          size="sm"
+          size="small"
           :text="showUnitsUpperInput ? 'hide range' : 'show range'"
           variant="text"
           @click="toggle"
         />]
       </div>
     </div>
-    <div class="d-flex">
+    <div class="align-center d-flex">
       <div class="pr-2">
-        <b-form-input
+        <v-text-field
           :id="inputId"
           :aria-invalid="!isValidUnits(unitsLower, max)"
           aria-labelledby="units-input-label"
-          class="units-input"
+          density="compact"
           :disabled="disable"
-          maxlength="4"
-          size="sm"
-          trim
-          :value="unitsLower"
-          @input="setUnitsLower"
-          @keypress.enter="onSubmit"
+          hide-details
+          maxlength="3"
+          min-width="60"
+          :model-value="unitsLower"
+          variant="outlined"
+          @keydown.enter="onSubmit"
           @keyup.esc="onEscape"
+          @update:model-value="setUnitsLower"
         />
       </div>
-      <div v-if="showUnitsUpperInput" class="pr-2 pt-1">
+      <div v-if="showUnitsUpperInput" class="pr-2">
         to
       </div>
       <div v-if="showUnitsUpperInput">
-        <b-form-input
+        <v-text-field
           :id="`upper-${inputId}`"
           :aria-invalid="!isValidUnits(unitsUpper)"
           aria-labelledby="units-input-label"
-          class="units-input"
+          density="compact"
           :disabled="disable"
-          maxlength="4"
-          size="sm"
-          trim
+          hide-details
+          maxlength="3"
+          min-width="60"
           :value="unitsUpper"
-          @input="setUnitsUpper"
-          @keypress.enter="onSubmit"
+          variant="outlined"
+          @keydown.enter="onSubmit"
+          @update:model-value="setUnitsUpper"
         />
       </div>
     </div>
-    <b-collapse :visible="!!errorMessage">
-      <span class="text-error text-grey font-size-12">
+    <v-expand-transition>
+      <div v-if="!!errorMessage" class="text-error text-grey font-size-12">
         {{ errorMessage }}
-      </span>
-    </b-collapse>
+      </div>
+    </v-expand-transition>
   </div>
 </template>
 
-<script>
-import Context from '@/mixins/Context'
-import DegreeEditSession from '@/mixins/DegreeEditSession'
-import Util from '@/mixins/Util'
-import {alertScreenReader} from '@/lib/utils'
+<script setup>
+import {alertScreenReader, putFocusNextTick} from '@/lib/utils'
 import {isValidUnits} from '@/lib/degree-progress'
+import {onMounted, ref} from 'vue'
 
-export default {
-  name: 'UnitsInput',
-  mixins: [Context, DegreeEditSession, Util],
-  props: {
-    disable: {
-      required: false,
-      type: Boolean
-    },
-    errorMessage: {
-      default: undefined,
-      required: false,
-      type: String
-    },
-    inputId: {
-      default: 'units-input',
-      required: false,
-      type: String
-    },
-    label: {
-      default: 'Units',
-      required: false,
-      type: String
-    },
-    labelClass: {
-      default: 'font-weight-500 mb-1 pr-2',
-      required: false,
-      type: String
-    },
-    max: {
-      default: 10,
-      required: false,
-      type: Number
-    },
-    onEscape: {
-      default: () => {},
-      required: false,
-      type: Function
-    },
-    onSubmit: {
-      required: true,
-      type: Function
-    },
-    range: {
-      required: false,
-      type: Boolean
-    },
-    required: {
-      required: false,
-      type: Boolean
-    },
-    setUnitsLower: {
-      required: true,
-      type: Function
-    },
-    setUnitsUpper: {
-      default: () => {},
-      required: false,
-      type: Function
-    },
-    unitsLower: {
-      default: undefined,
-      required: false,
-      type: [Number, String, undefined]
-    },
-    unitsUpper: {
-      default: undefined,
-      required: false,
-      type: [Number, String, undefined]
-    }
+const props = defineProps({
+  disable: {
+    required: false,
+    type: Boolean
   },
-  data: () => ({
-    showUnitsUpperInput: false
-  }),
-  created() {
-    this.showUnitsUpperInput = !!this.unitsUpper && this.unitsLower !== this.unitsUpper
-    if (!this.showUnitsUpperInput) {
-      this.setUnitsUpper(undefined)
-    }
+  errorMessage: {
+    default: undefined,
+    required: false,
+    type: String
   },
-  methods: {
-    isValidUnits,
-    toggle() {
-      this.showUnitsUpperInput = !this.showUnitsUpperInput
-      if (!this.showUnitsUpperInput) {
-        this.setUnitsUpper(undefined)
-      }
-      this.putFocusNextTick(this.showUnitsUpperInput && this.unitsLower ? `upper-${this.inputId}` : this.inputId)
-      alertScreenReader(`Enter ${this.showUnitsUpperInput ? 'end' : 'start'} value of range.`)
-    }
+  inputId: {
+    default: 'units-input',
+    required: false,
+    type: String
+  },
+  label: {
+    default: 'Units',
+    required: false,
+    type: String
+  },
+  labelClass: {
+    default: 'font-weight-500 mb-1 pr-2',
+    required: false,
+    type: String
+  },
+  max: {
+    default: 10,
+    required: false,
+    type: Number
+  },
+  onEscape: {
+    default: () => {},
+    required: false,
+    type: Function
+  },
+  onSubmit: {
+    required: true,
+    type: Function
+  },
+  range: {
+    required: false,
+    type: Boolean
+  },
+  required: {
+    required: false,
+    type: Boolean
+  },
+  setUnitsLower: {
+    required: true,
+    type: Function
+  },
+  setUnitsUpper: {
+    default: () => {},
+    required: false,
+    type: Function
+  },
+  unitsLower: {
+    default: undefined,
+    required: false,
+    type: [Number, String, undefined]
+  },
+  unitsUpper: {
+    default: undefined,
+    required: false,
+    type: [Number, String, undefined]
   }
+})
+
+const showUnitsUpperInput = ref(false)
+
+onMounted(() => {
+  showUnitsUpperInput.value = !!props.unitsUpper && props.unitsLower !== props.unitsUpper
+  if (!showUnitsUpperInput.value) {
+    props.setUnitsUpper(undefined)
+  }
+})
+
+const toggle = () => {
+  showUnitsUpperInput.value = !showUnitsUpperInput.value
+  if (!showUnitsUpperInput.value) {
+    props.setUnitsUpper(undefined)
+  }
+  putFocusNextTick(showUnitsUpperInput.value && props.unitsLower ? `upper-${props.inputId}` : props.inputId)
+  alertScreenReader(`Enter ${showUnitsUpperInput.value ? 'end' : 'start'} value of range.`)
 }
 </script>
-
-<style scoped>
-.toggle-btn {
-  padding: 0 1px 0 1px;
-}
-.units-input {
-  width: 3rem;
-}
-</style>
