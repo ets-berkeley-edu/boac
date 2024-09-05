b1f79f012381a1b5324e0452add7aefbd3b24fc9
Pauline Kerschen <kerschen@berkeley.edu>
2024-09-05 16:21:52 -0700

    NOJIRA v5.23.2 patch version bump

--- boac/__init__.py
+++ boac/__init__.py
@@ -29,7 +29,7 @@ from flask_caching import Cache
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
 
-__version__ = '5.23.2'
+__version__ = '5.23.3'
 
 db = SQLAlchemy()
 
