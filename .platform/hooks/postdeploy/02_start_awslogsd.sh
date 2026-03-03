f2d80664736f8d58d361fa6768f56bb7272144d9
DG <163147638+dg-ucb@users.noreply.github.com>
2026-03-03 10:29:52 -0500

    BOAC-6594, Enable Enhanced CloudWatch Metrics

--- .platform/hooks/postdeploy/02_start_awslogsd.sh
+++ .platform/hooks/postdeploy/02_start_awslogsd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/bin/config.json -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/tmp/amazon-cloudwatch-agent.json -s
